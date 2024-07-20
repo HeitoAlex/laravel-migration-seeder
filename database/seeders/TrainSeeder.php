948c095cf1389782d56e8f05436294a2635fbe75
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-07-20 18:09:11 +0200

    errore seeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i=0; $i < 10; $i++) {
+            $trains = new Train();
+            $trains->company = $faker->word();
+            $trains->departure_station = $faker->city();
+            $trains->arrival_station = $faker->city();
+            $trains->departure_time = $faker->dateTime();
+            $trains->arrival_time = $faker->dateTime();
+            $trains->train_code = $faker->randomNumber( );
+            $trains->wagons_no = $faker->randomNumber();
+            $trains->on_time = $faker->boolean();
+            $trains->deleted = $faker->boolean();
+            $trains->save();
+        }
+
+    }
+}
